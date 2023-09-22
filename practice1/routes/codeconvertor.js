2b70b285f2f417c7ca04b264ca4c1c1fd715b767
sharvarihupare-369 <sharvarihupare15@gmail.com>
2023-09-22 23:07:22 +0530

    add

--- practice1/routes/codeconvertor.js
+++ practice1/routes/codeconvertor.js
@@ -8,4 +8,5 @@ const openai = new OpenAI({
 const chatCompletion = await openai.chat.completions.create({
     messages: [{ role: "user", content: "Say this is a test" }],
     model: "gpt-3.5-turbo",
+    temperature : 7.0
 });
